8af0a53497722b60b7317d7d6badbdbf9d5a7e0a
Celis <celistek92@gmail.com>
2019-01-28 22:26:37 -0600

    resubir?

--- lab4/main.js
+++ lab4/main.js
@@ -1,3 +1,4 @@
+//ELEVAR NUMEROS DE 1 A N AL CUADRADO Y AL CUBO
 let num;
 num= prompt("Introduce un numero plox:")
 var i;
@@ -5,22 +6,28 @@ for(i=1;i<=num;i++){
     document.write(i+"--------"+ Math.pow(i,2)+ "--------"+ Math.pow(i,3))
     document.write("<br/>")
 }
+//generar los numeros aleatorios
 let rand1= Math.floor((Math.random()*300)+1);
 let rand2= Math.floor((Math.random()*300)+1);
+//crear una fecha al momento de generar los numeros
 ini_mseg= new Date();
-console.log(26);
+//console.log(26);
 let num2= prompt("¿Cuanto da la suma de "+rand1+" + "+rand2 +"?")
-console.log(num2);
+//console.log(num2);
+//Crear una nueva fecha al momento de recibir la respuesta
 fin_mseg=new Date();
+//hacer la resta de las fechas (se expresa en milisegundos)
 let tini=ini_mseg.getTime();
 let tend=fin_mseg.getTime();
 let a = (tend- tini)/1000;
+// Mostrar los mensajes de acuerdo a la respuesta
 if(num2==(rand1+rand2)){
     alert("CORRECTO, TARDASTE"+" "+ a + " SEGUNDOS");
 }
 else{
-    alert("INCORRECTO, ADEMÁS TE TARDASTE "+ a + "SEGUNDOS");
+    alert("INCORRECTO, LA RESPUESTA ES " + (rand1+rand2) +" ADEMÁS TE TARDASTE "+ a + "SEGUNDOS");
 }
+//Crear un arreglo con numeros aleatorios
 let nums = new Array(20);
 for(i=0;i<20;i++){
     nums[i]=Math.floor((Math.random()*42)-20);
@@ -29,7 +36,9 @@ for(i=0;i<20;i++){
 let pos=0,neg=0,ceros=0;
 document.write("<br/>")
 function contador(arre){
+    //escribir el arreglo en el documento
     document.write("El arreglo es el siguiente:")
+    //contar lo que se pide
     for(i=0;i<=arre.length-1;i++){
         document.write(arre[i]+" ,")
         if(arre[i]<0){
@@ -47,9 +56,11 @@ document.write("ceros: "+ceros+ "<br/>");
 document.write("positivos: "+pos+"<br/>");
 }
 contador(nums);
+//CREAR UN ARREGLO
 let reng= new Array(10);
 let j;
 for(i=0;i<10;i++){
+    //CADA POSICIÓN DEL ARREGLO ES UN NUEVO ARREGLO
     reng[i]=new Array(10)
     for(j=0;j<10;j++){
         reng[i][j]=Math.floor((Math.random()*20)+1);
@@ -58,6 +69,7 @@ for(i=0;i<10;i++){
 document.write("<br/>")
 document.write("<br/>")
 document.write("MATRIZ: <br/>")
+//ESCRIBIR LA MATRIZ
 for(i=0;i<10;i++){
     for(j=0;j<10;j++){
     document.write(reng[i][j]+"|");
@@ -65,6 +77,7 @@ for(i=0;i<10;i++){
     document.write("<br/>");
 }
 let acum=0;
+//promedio por renglón
 function promedio(arra){
     for(i=0;i<10;i++){
         acum=0;
@@ -75,6 +88,7 @@ function promedio(arra){
     }
 }
 promedio(reng);
+//INVERTIR NUMERO RECIBIDO
 function invertir(numero)
 { 
   let invertido = 0;
@@ -94,29 +108,30 @@ function primo(x)
     let i;
     if(x<2)
         return false ;
-    for(i=2 ; i<=sqrt(x); i++)
+    for(i=2 ; i<=Math.sqrt(x); i++)
     {
         if(x%i==0)
             return false ;
     }
     return true ;
 }
+document.write("<br/>"+"<br/>"+"RESPUESTAS AL PROBLEMA"+"<br/>")
 function principal(){
     let rain ;
-     //let n=promt("Ingresa un numero")
-    let rep=n;
-    while(n>0)
+     let n=prompt("Ingresa la cantidad de números a analizar")
+    while(n--)
     {
-        //let num=prompt("ingresa numeros");
+        let num=prompt("Ingresa un numero");
         rain=Math.floor(Math.sqrt(num));
         if(num!=(rain*rain))
-            document.write("NO");
+            document.write(num +"¿Es un T-primo?: NO"+"<br/>");
         else if(!primo(rain))
-            document.write("NO");
+            document.write(num +"¿Es un T-primo?: NO"+"<br/>");
         else if(primo(rain))
-            document.write("YES");
+            document.write(num+"¿Es un T-primo?: YES"+"<br/>");
     }
 } 
+principal();
 
 
 
