6874e9bc9913fb2fdf8ca553359c6751cb683a1a
Celis <celistek92@gmail.com>
2019-01-31 22:53:41 -0600

    Creación del documento productoshtml y carpeta img

--- lab5/script.js
+++ lab5/script.js
@@ -1,50 +1,56 @@
 
-window.onload=iniciar;
-function iniciar(){
-    document.getElementById("enviar").addEventListener('click',validar,false);
-}
+// window.onload=iniciar;
+// function iniciar(){
+//     document.getElementById("enviar").addEventListener('click',validar,false);
+// }
 function validaContra(){
     let elemento=document.getElementById("contra");
+    let pas1=document.getElementById("contra").value;
+    let pas2=document.getElementById("contravali").value;
+    console.log(pas1);
+    console.log(pas2);
+    let res1,res2,res3,res4,resmax;
+    let espacios=false;
+    let conta=0;
     if(!elemento.checkValidity()){
-        error(elemento);
+        res1=false;
+        console.log("res1: "+ res1);
         return false;
     }
+    while(espacios&&(conta<pas1.length)){
+        if(pas1.charAt(conta)==" ")
+            espacios=true;
+        conta ++;
+    }
+    if(espacios){
+        alert("La contraseña no puede tener espacios")
+        res2=false;
+        console.log("res2: "+ res2);
+        return false;
+    }
+    if (pas1.length == 0 || pas2.length == 0) {
+        alert("Los campos de la password no pueden quedar vacios");
+        res3=false;
+        console.log("res3: "+ res3);
+        return false;
+      }
+      if (p1 != p2) {
+        alert("Las passwords deben de coincidir");
+        res4=false;
+        console.log("res4: "+ res4);
+        return false;
+      }
+      resmax=true;
+        console.log("resmax: "+ resmax);
+
     return true;
 }
 function validar(e){
-    if(validaContra()&& validaPwd() &&confirm("Pulsa aceptar si quieres enviar el formulario"))
+    if(validaContra() && confirm("Pulsa aceptar si quieres enviar el formulario"))
     {
         return true;
     }else{
         e.preventDefault();
         return false;
     }
-}
-function validaPwd(){
-    let pas1=document.getElementById("contra").value;
-let pas2=document.getElementById("contravali").value;
-console.log(pas1);
-console.log(pas2);
-let espacios=false;
-let conta=0;
-while(espacios&&(conta<pas1.length)){
-    if(pas1.charAt(cont)==" ")
-        espacios=true;
-    conta ++;
-}
-if(espacios){
-    alert("La contraseña no puede tener espacios")
-    return false;
-}
-if (pas1.length == 0 || pas2.length == 0) {
-    alert("Los campos de la password no pueden quedar vacios");
-    return false;
-  }
-  if (p1 != p2) {
-    alert("Las passwords deben de coincidir");
-    return false;
-  } else {
-    alert("Todo bien. Puede usted pasar");
-    return true; 
-  }
 }
